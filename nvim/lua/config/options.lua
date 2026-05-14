a6ddc5b15ad184653b7b4c6f1f89aafb86368602
Thomas Watts <tawatts3@gmail.com>
2026-05-14 00:52:48 -0400

    fix some lua config and path stuff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus"
+
+-- mise shims aren't on PATH in non-login shells; add them so node and other mise tools work
+vim.env.PATH = vim.fn.expand("~/.local/share/mise/shims") .. ":" .. vim.env.PATH
